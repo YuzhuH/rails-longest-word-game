10b88a10e482dc5b3ff4c3fea1d6821ea2fa60e8
Yuzhu Hou <yuzhu.hou@gmail.com>
2017-11-27 16:46:17 +0100

    longest word game

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/play', to: "games#play"
+  post '/scores', to: "games#scores"
+  get '/', to: "games#play"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
